a28fe64100f79f3d57d423e1ba1a5e9fd596a15d
Ragib Shariar <srragib@gmail.com>
2022-05-17 21:55:39 +0600

    try from the scratch

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// use middleware
 app.use(cors());
 app.use(express.json());
 
-// username: user1
+// username: dbuser1
 //pass: 1234
 
+
+const uri = "mongodb+srv://dbuser1:1234@cluster0.zlmws.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  
+}
+// client.connect(err => {
+//   const collection = client.db("RagibInventory").collection("perfumes");
+//   // console.log("Database Connected");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
 app.get('/', (req, res) => {
-    res.send('NODEMON installed');
+    res.send('Ragib Perfume Arena Inventory Server');
 });
 
 app.listen(port, ()=>{
