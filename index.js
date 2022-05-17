90c6480a9d1e863d2285c8ea8488893ae03c6037
Ragib Shariar <srragib@gmail.com>
2022-05-18 01:02:28 +0600

    inventory updated

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { config } = require('dotenv');
 const app = express();
+// const jwt = require('jsonwebtoken');
+
 const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
-// username: dbuser1
-//pass: 1234
-
-
-const uri = "mongodb+srv://dbuser1:1234@cluster0.zlmws.mongodb.net/?retryWrites=true&w=majority";
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zlmws.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
   try {
     await client.connect();
-    // console.log('db connected')
+    // console.log('db connected');
 
-    const productCollection = client.db("RagibInventory").collection("perfumes");
-    const orderCollection = client.db("RagibInventory").collection("orders");
+    const productCollection = client.db("perfume_inventory").collection("products");
+    const orderCollection = client.db("perfume_inventory").collection("orders");
 
     //all products
     app.get("/products", async (req, res) => {
         const query = {};
         const products = await productCollection.find(query).toArray();
-        // console.log('inside products ->', products)
+        console.log('inside products ->', products);
         res.send(products);
-    })
+    });
 
-    //single product
-    app.get("/product/:id", async (req, res) => {
+    // single product
+    app.get("/products/:id", async (req, res) => {
         const id = req.params.id;
         const product = await productCollection.findOne({ _id: ObjectId(id) });
         res.send(product);
@@ -69,12 +70,12 @@ async function run(){
         res.send(result);
     })
 
-    //jwt token while logging in
-    app.post("/login", (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-        res.send({ token })
-    })
+    // jwt token while logging in
+    // app.post("/login", (req, res) => {
+    //     const user = req.body;
+    //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    //     res.send({ token })
+    // })
 
     //add to order
     app.post("/add-order", async (req, res) => {
@@ -84,24 +85,24 @@ async function run(){
     })
 
     //order list
-    app.get("/order-list", verifyToken, async (req, res) => {
-        const decodedEmail = req.decoded.email;
-        const email = req.query.email;
-        if (decodedEmail === email) {
-            const orders = await orderCollection.find({ email }).toArray();
-            res.send(orders);
-        } else {
-            res.status(403).send({ message: 'You are forbidden, if you try again I\'m gonna call the cops' })
-        }
-    })
+    // app.get("/order-list", verifyToken, async (req, res) => {
+    //     const decodedEmail = req.decoded.email;
+    //     const email = req.query.email;
+    //     if (decodedEmail === email) {
+    //         const orders = await orderCollection.find({ email }).toArray();
+    //         res.send(orders);
+    //     } else {
+    //         res.status(403).send({ message: 'You are forbidden, if you try again I\'m gonna call the cops' })
+    //     }
+    // })
 
-    //delete from order
-    app.delete('/order-list/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await orderCollection.deleteOne(query);
-        res.send(result);
-    })
+    // delete from order
+    // app.delete('/order-list/:id', async (req, res) => {
+    //     const id = req.params.id;
+    //     const query = { _id: ObjectId(id) };
+    //     const result = await orderCollection.deleteOne(query);
+    //     res.send(result);
+    // })
 
 } finally {
     //   await client.close();
@@ -111,7 +112,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Ragib Perfume Arena Inventory Server');
+    res.send('Ragib Perfume Inventory Server...');
 });
 
 app.listen(port, ()=>{
