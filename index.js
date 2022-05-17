49ad7ce2e158b8ac6d9d42856ae55988907e69b3
Ragib Shariar <srragib@gmail.com>
2022-05-17 22:01:17 +0600

    crud operation

--- index.js
+++ index.js
@@ -16,15 +16,98 @@ const uri = "mongodb+srv://dbuser1:1234@cluster0.zlmws.mongodb.net/?retryWrites=
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
-  
-}
-// client.connect(err => {
-//   const collection = client.db("RagibInventory").collection("perfumes");
-//   // console.log("Database Connected");
-//   // perform actions on the collection object
-//   client.close();
-// });
+  try {
+    await client.connect();
+    // console.log('db connected')
 
+    const productCollection = client.db("RagibInventory").collection("perfumes");
+    const orderCollection = client.db("RagibInventory").collection("orders");
+
+    //all products
+    app.get("/products", async (req, res) => {
+        const query = {};
+        const products = await productCollection.find(query).toArray();
+        // console.log('inside products ->', products)
+        res.send(products);
+    })
+
+    //single product
+    app.get("/product/:id", async (req, res) => {
+        const id = req.params.id;
+        const product = await productCollection.findOne({ _id: ObjectId(id) });
+        res.send(product);
+    })
+
+    //updating a product
+    app.put('/update-product/:id', async (req, res) => {
+        const id = req.params.id;
+        const updatedBikeInfo = req.body;
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updatedDoc = {
+            $set: {
+                quantity: updatedBikeInfo.quantity,
+                sold: updatedBikeInfo.sold
+            }
+        }
+        const result = await productCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+    })
+
+    //deleting a product
+    app.delete('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    //adding a product
+    app.post("/add-product", async (req, res) => {
+        const bikeInfo = req.body;
+        const result = await productCollection.insertOne(bikeInfo);
+        res.send(result);
+    })
+
+    //jwt token while logging in
+    app.post("/login", (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+        res.send({ token })
+    })
+
+    //add to order
+    app.post("/add-order", async (req, res) => {
+        const orderInfo = req.body;//=> ...bike, email
+        const result = await orderCollection.insertOne(orderInfo);
+        res.send({ success: 'order complete' })
+    })
+
+    //order list
+    app.get("/order-list", verifyToken, async (req, res) => {
+        const decodedEmail = req.decoded.email;
+        const email = req.query.email;
+        if (decodedEmail === email) {
+            const orders = await orderCollection.find({ email }).toArray();
+            res.send(orders);
+        } else {
+            res.status(403).send({ message: 'You are forbidden, if you try again I\'m gonna call the cops' })
+        }
+    })
+
+    //delete from order
+    app.delete('/order-list/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
+        res.send(result);
+    })
+
+} finally {
+    //   await client.close();
+}
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
